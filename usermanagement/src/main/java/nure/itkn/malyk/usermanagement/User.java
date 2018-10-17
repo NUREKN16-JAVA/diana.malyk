59ba5617b9c437a56b4215a21abd53a419eb3a82
Diana Malyk <diana.malyk@nure.ua>
2018-10-17 21:35:36 +0300

    The end

--- usermanagement/src/main/java/nure/itkn/malyk/usermanagement/User.java
+++ usermanagement/src/main/java/nure/itkn/malyk/usermanagement/User.java
@@ -1,4 +1,5 @@
 package nure.itkn.malyk.usermanagement;
+import java.util.Calendar;
 import java.util.Date;
 import java.io.Serializable;
 
@@ -9,12 +10,18 @@ class User implements Serializable {
 	private String lastName;
 	private Date dateOfBirth;
 	
+	public User() {
+	  firstName = "";
+	  lastName = "";
+	}
+	
 	public User(Long id, String firstName, String lastName, Date dateOfBirth) {
 		setId(id);
 		setFirstName(firstName);
 		setLastName(lastName);
 		setDateOfBirth(dateOfBirth);
 	}
+	
 	
 	/**
 	 * @return the id
@@ -64,6 +71,45 @@ class User implements Serializable {
 	public void setDateOfBirth(java.util.Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
-
 	
+	/**
+   * public String getFullName()
+   * requires: нет ограничений
+   * effects: полное имя пользоватея в формате lastName + " ," + firstName.
+   */
+	public String getFullName() {
+	  StringBuilder fullName = new StringBuilder();
+	  fullName.append(this.lastName);
+	  fullName.append(", ");
+	  fullName.append(this.firstName);
+	  return fullName.toString();
+	}
+	
+	/**
+   * public int getAge()
+   * requires: нет ограничений
+   * effects: возраст пользователя. 
+   * Если дата рождения больше текущей даты, выбрасывается исключение IllegalArgumentException
+   */
+	public int getAge() {
+	  Calendar current_date = Calendar.getInstance();
+	  current_date.setTime(new Date());
+	  Calendar birth_date = Calendar.getInstance();
+	  birth_date.setTime(this.getDateOfBirth());
+	  
+	  if(birth_date.after(current_date)) {
+	    throw new IllegalArgumentException("bad date");
+	  }
+	  
+	  int age = current_date.get(Calendar.YEAR) - birth_date.get(Calendar.YEAR);
+	  if(birth_date.get(Calendar.MONTH) <= current_date.get(Calendar.MONTH)) {
+	    if (birth_date.get(Calendar.DAY_OF_MONTH) <= current_date.get(Calendar.DAY_OF_MONTH)) {
+	      return age;
+	    } else {
+	      return (age-1);
+	    }
+	  } else {
+	    return (age-1);
+	  }
+	}
 }
--- usermanagement/src/test/java/nure/itkn/malyk/usermanagement/TestUserClass.java
+++ usermanagement/src/test/java/nure/itkn/malyk/usermanagement/TestUserClass.java
@@ -1,15 +1,70 @@
 package nure.itkn.malyk.usermanagement;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import junit.framework.TestCase;
 
-class TestUserClass extends TestCase {
+public class TestUserClass extends TestCase {
 	private User user;
+	
+	public TestUserClass() {
+	  super();
+	  try {
+	    setUp();
+	  } catch (Exception e) {
+	    e.printStackTrace();
+	  }
+	}
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		user = new User(1L, "Иван", "Иванов", new SimpleDateFormat("d-MMM-yyyy").parse("29-Apr-2018"));
+		user = new User(Long.valueOf(1L), "Иван", "Иванов", new SimpleDateFormat("dd-MM-yyyy").parse("18-12-1998"));
+		
 	}
+	
+	
+	public void testGetFullName() {
+	  assertEquals("Иванов, Иван", user.getFullName());
+	}
+	
 
+	
+	public void testGetAge() {
+	  boolean isThrown = false;
+	  try {
+	    //18.12.1998
+	    assertEquals(19, user.getAge());
+	    
+	    Calendar calendar = Calendar.getInstance();
+	    
+      calendar.setTime(new java.util.Date());
+	    user.setDateOfBirth(calendar.getTime());
+	    assertEquals(0, user.getAge());
+	    
+	    calendar.add(Calendar.DATE, 1);
+      user.setDateOfBirth(calendar.getTime());
+      user.getAge();
+	    
+      calendar.setTime(new java.util.Date());
+      calendar.set(Calendar.YEAR, 1998);
+      user.setDateOfBirth(calendar.getTime());
+      assertEquals(20, user.getAge());
+      
+      calendar.add(Calendar.DATE, 1);
+      user.setDateOfBirth(calendar.getTime());
+      assertEquals(19, user.getAge());
+      
+      calendar.add(Calendar.DATE, -2);
+      user.setDateOfBirth(calendar.getTime());
+      assertEquals(20, user.getAge());
+      
+	  } catch(IllegalArgumentException ex) {
+	    isThrown = true;
+	    }
+	    catch(Exception e) {
+	      e.printStackTrace();
+	    }
+	  assertTrue(isThrown);
+	 }
 }
