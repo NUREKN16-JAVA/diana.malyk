f1f257fe9c64986eb3bfc2155eb221644779c3d2
Diana Malyk <diana.malyk@nure.ua>
2018-10-18 08:54:09 +0300

    Add method equals and hashcode. Update serialVersionUID

--- usermanagement/src/main/java/nure/itkn/malyk/usermanagement/User.java
+++ usermanagement/src/main/java/nure/itkn/malyk/usermanagement/User.java
@@ -4,11 +4,65 @@ import java.util.Date;
 import java.io.Serializable;
 
 class User implements Serializable {
-	private static final long serialVersionUID = -4186523343377560524L;
-	private Long id;
-	private String firstName;
-	private String lastName;
-	private Date dateOfBirth;
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 6516265594737505284L;
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private Date dateOfBirth;
+  
+	/* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+    result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    User other = (User) obj;
+    if (dateOfBirth == null) {
+      if (other.dateOfBirth != null)
+        return false;
+    } else if (!dateOfBirth.equals(other.dateOfBirth))
+      return false;
+    if (firstName == null) {
+      if (other.firstName != null)
+        return false;
+    } else if (!firstName.equals(other.firstName))
+      return false;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (lastName == null) {
+      if (other.lastName != null)
+        return false;
+    } else if (!lastName.equals(other.lastName))
+      return false;
+    return true;
+  }
+
+
 	
 	public User() {
 	  firstName = "";
