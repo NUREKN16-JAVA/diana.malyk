ff271bb86858bd03f3f0cce9d827b9539af40a02
Diana Malyk <diana.malyk@nure.ua>
2018-11-13 17:21:29 +0200

    Update method User.create

--- usermanagement/src/main/java/nure/itkn/malyk/usermanagement/db/HsqldbUserDao.java
+++ usermanagement/src/main/java/nure/itkn/malyk/usermanagement/db/HsqldbUserDao.java
@@ -1,11 +1,15 @@
 package nure.itkn.malyk.usermanagement.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
 
 import nure.itkn.malyk.usermanagement.User;
 
 public class HsqldbUserDao implements UserDao {
 
+	private static final String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES (?, ?, ?)";
 	private ConnectionFactory connectionFactory;
 	
 	public HsqldbUserDao(ConnectionFactory connectionFactory) {
@@ -18,8 +22,20 @@ public class HsqldbUserDao implements UserDao {
 
 	@Override
 	public User create(User user) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Connection connection = connectionFactory.createConnection();
+			PreparedStatement statement = connection
+					.prepareStatement(INSERT_QUERY);
+			statement.setString(1, user.getFirstName());
+			statement.setString(2, user.getLastName());
+			statement.setDate(2, new java.sql.Date(user.getDateOfBirth().getTime()));
+			int n = statement.executeUpdate()
+			return null;
+		} catch (DatabaseException e) {
+			throw e;
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
 	}
 
 	@Override
