c3fca2905b4cc14e51066ffec0ea6177c58a3573
Diana Malyk <diana.malyk@nure.ua>
2018-11-29 20:45:49 +0200

    Set up file settings. Add class DaoFactoryImpl. Modify class DaoFactory.

--- usermanagement/src/main/java/nure/itkn/malyk/usermanagement/db/ConnectionFactoryImpl.java
+++ usermanagement/src/main/java/nure/itkn/malyk/usermanagement/db/ConnectionFactoryImpl.java
@@ -3,6 +3,7 @@ package nure.itkn.malyk.usermanagement.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactoryImpl implements ConnectionFactory {
 	private String url;
@@ -15,6 +16,13 @@ public class ConnectionFactoryImpl implements ConnectionFactory {
 		this.driver = driver;
 		this.user = user;
 		this.password = password;
+	}
+
+	public ConnectionFactoryImpl(Properties properties) {
+		String user = properties.getProperty("connection.user");
+		String password = properties.getProperty("connection.password");
+		String url = properties.getProperty("connection.url");
+		String driver = properties.getProperty("connection.driver");
 	}
 
 	@Override
