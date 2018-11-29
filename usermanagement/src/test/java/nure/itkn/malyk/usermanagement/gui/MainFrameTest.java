98c9f18eb0b21b93a92fb3e06d931cc7a9ca00b8
Diana Malyk <diana.malyk@nure.ua>
2018-11-29 21:55:28 +0200

    Add metohd find to MainClassTest

--- usermanagement/src/test/java/nure/itkn/malyk/usermanagement/gui/MainFrameTest.java
+++ usermanagement/src/test/java/nure/itkn/malyk/usermanagement/gui/MainFrameTest.java
@@ -1,7 +1,10 @@
 package nure.itkn.malyk.usermanagement.gui;
 
+import java.awt.Component;
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
+import junit.extensions.jfcunit.finder.NamedComponentFinder;
 
 public class MainFrameTest extends JFCTestCase {
 
@@ -19,5 +22,12 @@ public class MainFrameTest extends JFCTestCase {
 		getHelper().cleanUp(this);
 		super.tearDown();
 	}
-
+	
+	private Component find(Class componentClass, String name) {
+		NamedComponentFinder finder;
+		finder = new NamedComponentFinder(componentClass, name);
+		Component component = finder.find(mainFrame, 0);
+		assertNotNull("Could not find component '" + name + "'", component);
+		return component;
+	}
 }
