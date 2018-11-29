dcf7e1ead7927a458b0ff427ccc62dcd9632044b
Diana Malyk <diana.malyk@nure.ua>
2018-11-29 22:05:32 +0200

    Add test for components of main window

--- usermanagement/src/test/java/nure/itkn/malyk/usermanagement/gui/MainFrameTest.java
+++ usermanagement/src/test/java/nure/itkn/malyk/usermanagement/gui/MainFrameTest.java
@@ -2,6 +2,10 @@ package nure.itkn.malyk.usermanagement.gui;
 
 import java.awt.Component;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
 import junit.extensions.jfcunit.finder.NamedComponentFinder;
@@ -26,8 +30,18 @@ public class MainFrameTest extends JFCTestCase {
 	private Component find(Class componentClass, String name) {
 		NamedComponentFinder finder;
 		finder = new NamedComponentFinder(componentClass, name);
+		finder.setWait(0);
 		Component component = finder.find(mainFrame, 0);
 		assertNotNull("Could not find component '" + name + "'", component);
 		return component;
+	}
+	
+	public void testBrowseControls() {
+		find(JPanel.class, "browsePanel");
+		find(JTable.class, "userTable");
+		find(JButton.class, "addButton");
+		find(JButton.class, "editButton");
+		find(JButton.class, "deleteButton");
+		find(JButton.class, "detailsButon");
 	}
 }
