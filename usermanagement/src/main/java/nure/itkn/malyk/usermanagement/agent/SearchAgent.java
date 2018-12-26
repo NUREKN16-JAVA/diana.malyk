4de126d2c525645939d5fc0ce8a16bc1e85d8ef1
Diana Malyk <diana.malyk@nure.ua>
2018-12-26 23:31:42 +0200

    Add method search for seach agent

--- usermanagement/src/main/java/nure/itkn/malyk/usermanagement/agent/SearchAgent.java
+++ usermanagement/src/main/java/nure/itkn/malyk/usermanagement/agent/SearchAgent.java
@@ -1,6 +1,10 @@
 package nure.itkn.malyk.usermanagement.agent;
 
+import java.util.Collection;
+
 import jade.core.Agent;
+import nure.itkn.malyk.usermanagement.db.DaoFactory;
+import nure.itkn.malyk.usermanagement.db.DatabaseException;
 
 public class SearchAgent extends Agent {
 
@@ -22,4 +26,21 @@ public class SearchAgent extends Agent {
 		super.takeDown();
 	}
 
+	public void search(String firstName, String lastName) throws SearchException {
+		try {
+			Collection users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+			if(users.size() > 0) {
+				showUsers(users);
+			} else {
+				
+			}
+			
+		} catch (DatabaseException e) {
+			throw new SearchException(e);
+		}
+	}
+	
+	public void showUsers(Collection users) {
+		
+	}
 }
---
+++ usermanagement/src/main/java/nure/itkn/malyk/usermanagement/agent/SearchException.java
@@ -0,0 +1,11 @@
+package nure.itkn.malyk.usermanagement.agent;
+
+import nure.itkn.malyk.usermanagement.db.DatabaseException;
+
+public class SearchException extends Exception {
+
+	public SearchException(DatabaseException e) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
